3f0749a690457f7417a9df72239d34aa153d7709
Gutu Galuppo <galuppodev@gmail.com>
2025-08-04 13:22:51 +0200

    add Igor pic

--- src/components/Quotes/quotesData.ts
+++ src/components/Quotes/quotesData.ts
@@ -3,6 +3,7 @@ import placeholder02 from "../../assets/images/placeholder02_pic.png";
 import placeholder03 from "../../assets/images/placeholder03_pic.png";
 import antonios from "../../assets/images/quoteSection/Antonios02.png";
 import lem from "../../assets/images/quoteSection/Lem03.jpg";
+import igor from "../../assets/images/quoteSection/IgorBG.png";
 
 interface Quote {
 	picture: string;
@@ -31,27 +32,27 @@ export const quotesData: Quote[] = [
     He is also eager to learn new technologies, which helps drive innovation within the team.”`,
 	},
 	{
-		picture: placeholder02,
-		collegueName: "Hugo Nogueira",
-		company: "Tilda",
-		description: `“A colaboração com Gutu sempre flui naturalmente.
-    Ele entende as limitações técnicas do backend e propõe soluções inteligentes no frontend. Além disso, seu código é limpo, bem documentado e fácil de manter.
-    Um verdadeiro parceiro de equipe!”`,
-	},
-	{
-		picture: placeholder03,
-		collegueName: "Feli",
-		company: "Super Vista",
-		description: `“Gutu tem uma sensibilidade única para transformar protótipos em experiências reais.
-    Ele não apenas implementa, mas também melhora a visão do design com sugestões valiosas.
-    A atenção que ele dá à acessibilidade faz toda a diferença!”`,
-	},
-	{
-		picture: placeholder,
+		picture: igor,
 		collegueName: "Igor Santos",
 		company: "Super Vista",
 		description: `“Gutu tem uma sensibilidade única para transformar protótipos em experiências reais.
       Ele não apenas implementa, mas também melhora a visão do design com sugestões valiosas.
       A atenção que ele dá à acessibilidade faz toda a diferença!”`,
 	},
+	// {
+	// 	picture: placeholder02,
+	// 	collegueName: "Hugo Nogueira",
+	// 	company: "Tilda",
+	// 	description: `“A colaboração com Gutu sempre flui naturalmente.
+	//   Ele entende as limitações técnicas do backend e propõe soluções inteligentes no frontend. Além disso, seu código é limpo, bem documentado e fácil de manter.
+	//   Um verdadeiro parceiro de equipe!”`,
+	// },
+	// {
+	// 	picture: placeholder03,
+	// 	collegueName: "Feli",
+	// 	company: "Super Vista",
+	// 	description: `“Gutu tem uma sensibilidade única para transformar protótipos em experiências reais.
+	//   Ele não apenas implementa, mas também melhora a visão do design com sugestões valiosas.
+	//   A atenção que ele dá à acessibilidade faz toda a diferença!”`,
+	// },
 ];
